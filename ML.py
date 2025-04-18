3422204795cfa6657c5249032b6dd1c5bbd708c0
annakorda <korda.anna01@gmail.com>
2025-04-18 20:25:13 +0200

    Saving my work before pulling

--- ML.py
+++ ML.py
@@ -1,20 +1,23 @@
 from utils.classes import *
 import utils.check_files as cf 
 import utils.feature_extraction as fe 
+import numpy as np
 import sys
 import os
+from pathlib import Path
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2:
-        print("Usage: python ML.py <file_with_pdb_names.txt>")
+    if len(sys.argv) != 3:
+        print("Usage: python ML.py <file_with_pdb_names.txt> <output_file_name.npy>")
         sys.exit(1)
     
     pdb_list_file = sys.argv[1]
+    output_name = sys.argv[2]  # just the name, not full path
 
     if not cf.file_exists(pdb_list_file):
         sys.exit(1)
 
-    if not cf.can_read_file:
+    if not cf.can_read_file(pdb_list_file):
         sys.exit(1)
 
     proteins = []
@@ -30,10 +33,25 @@ if __name__ == "__main__":
                 print(f"{pdb_dir} will be skipped.")
             else:
                 proteins.append(Protein(pdb_dir, pdb_name))
-    file.close()
 
+    all_vectors = []
+    processed_count = 0
     for protein in proteins:
-        print(fe.generate_vector(protein))
-        
-        
-        
+        vectors = fe.generate_vector(protein, ML=True)
+        if vectors is None:
+          continue
+        all_vectors.extend(vectors)
+        processed_count += 1
+
+    # Create input_numpy directory in current folder if not exists
+    output_dir = Path.cwd() / "input_numpy"
+    output_dir.mkdir(exist_ok=True)
+
+    # Save final .npy file into that directory
+    output_path = output_dir / output_name
+    np.save(output_path, np.array(all_vectors))
+    print(f"Saved {len(all_vectors)} feature vectors to: {output_path}")
+
+    print(f"\nSuccessfully processed {processed_count} out of {len(proteins)} PDBs.")
+
+
