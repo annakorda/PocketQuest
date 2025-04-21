f2d37c018ca6510c1d15fbfe18ff694821f6c309
Montse <mgcerqueda@gmail.com>
2025-04-21 18:37:57 +0200

    small changes

--- PocketQuest.py
+++ PocketQuest.py
@@ -3,15 +3,18 @@ import utils.check_files as cf
 import utils.feature_extraction as fe 
 import utils.clustering as cl
 import utils.make_pdb as pdb
-import utils.visualization as vis
+# import numpy as np
 import sys
 import os
+# from pathlib import Path
+
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Usage: python PocketQuest.py <protein.pdb> [--vis N] [--rotate]")
+    # temp measure
+    if len(sys.argv) != 2:
+        print("Usage: python PocketQuest.py <protein.pdb> ")
         sys.exit(1)
-
+    
     pdb_dir = sys.argv[1]
     pdb_base_name = os.path.basename(pdb_dir)
     pdb_name = os.path.splitext(pdb_base_name)[0]
